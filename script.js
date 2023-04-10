5055bb024337ea654c8c68dcfaf345a1f02a162f
Eduardo Almeida <edu.almeida8@gmail.com>
2023-04-10 11:06:04 +0100

    Finished

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function start(n = 16) {
-  drawGrid(n)
-}
-
-function drawGrid(numberOfRows) {
+function drawGrid(numberOfRows = 16) {
   let container = document.querySelector('.container')
+
+  container.replaceChildren()
+
   for (let i = 1; i <= numberOfRows * numberOfRows; i++) {
     let cell = document.createElement('div')
     cell.classList.add('cell')
@@ -11,8 +10,28 @@ function drawGrid(numberOfRows) {
     let percentage = `${100 / numberOfRows}%`
     cell.style.flexBasis = percentage
 
+    cell.addEventListener('mouseenter', paintCellHandler)
+
     container.appendChild(cell)
   }
 }
 
-start()
+function paintCellHandler(event) {
+  let color =
+    '#' +
+    Math.floor(Math.random() * (0xffffff + 1))
+      .toString(16)
+      .padStart(6, '0')
+  event.target.style.backgroundColor = `${color}`
+}
+
+function newGridButtonHandler() {
+  let gridSize = prompt("What's the new size?", 16)
+  if (gridSize > 100) {
+    alert('The maximum number of rows of 100')
+  } else {
+    drawGrid(gridSize)
+  }
+}
+
+drawGrid()
